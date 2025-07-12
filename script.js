e20ff09043c3beed41fe05bf72e64ed28472e757
Gannitto <danilaserezhin@gmail.com>
2025-07-12 14:31:20 +0200

    Вернул загрузку модели

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const maxLen = 152; // Должно совпадать с max_sequence_len из обучения
 
 // Загрузка модели
 async function loadModel() {
-	//const model = await tf.loadLayersModel('tfjs_model/model.json');
+	const model = await tf.loadLayersModel('tfjs_model/model.json');
 	const tokenizerFile = await fetch('./tfjs_model/tokenizer.json')
 	console.log(tokenizerFile)
 	const tokenizerText = await tokenizerFile.text();
